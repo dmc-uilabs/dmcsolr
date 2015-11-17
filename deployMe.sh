ab6dda9a8b2aa2671e05ee0e5563b855fc6336c7
Bowden Wise <wisegb@ge.com>
2015-11-17 10:57:39 -0500

    Updates to deployMe.sh to support SolR 5.3.1 configs

--- deployMe.sh
+++ deployMe.sh
@@ -4,7 +4,7 @@
 #
 
 #
-yum update -y
+sudo yum update -y
 
 # install java
 echo installing java 8
@@ -43,22 +43,22 @@ sudo mv -f /tmp/solr/LuceneSolrConfig/* /opt/solr/solr-5.3.1/server/solr
 
 # Edit components.data-config.xml
 cd /tmp/dmcsolr
-sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/components.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/components/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/components.data-config.xml > /opt/solr/solr-5.3.1/server/solr/gforge/components/conf/data-config.xml
 
 # Edit projects.data-config.xml
-sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/projects.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/projects/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/projects.data-config.xml > /opt/solr/solr-5.3.1/server/solr/gforge/projects/conf/data-config.xml
 
 # Edit services.data-config.xml
-sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/services.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/services/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/services.data-config.xml > /opt/solr/solr-5.3.1/server/solr/gforge/services/conf/data-config.xml
 
 # Edit users.data-config.xml
-sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/users.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/users/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/users.data-config.xml > /opt/solr/solr-5.3.1/server/solr/gforge/users/conf/data-config.xml
 
 # Edit wiki.data-config.xml
-sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/wiki.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/wiki/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/wiki.data-config.xml > /opt/solr/solr-5.3.1/server/solr/gforge/wiki/conf/data-config.xml
 
 #
 echo starting solr
-sudo /opt/solr/apache-solr-3.6.0/bin/solr start
+sudo /opt/solr/solr-5.3.1/bin/solr start
 
 
