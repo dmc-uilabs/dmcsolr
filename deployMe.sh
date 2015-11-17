df3feaf48612be57a94c9d5ca7fb784e8b0a2a8b
Bowden Wise <wisegb@ge.com>
2015-11-17 09:41:49 -0500

    Updates to deployMe.sh to support SolR 5.3.1 configs

--- deployMe.sh
+++ deployMe.sh
@@ -3,75 +3,62 @@
 # deployMe.sh for Apache Solr
 #
 
-cd /tmp
-rm -fr /tmp/dmcsolr
-git clone https://bitbucket.org/DigitalMfgCommons/dmcsolr.git
-cd dmcsolr
-# mv * ..
-
 #
 yum update -y
 
 # install java
-echo installing java
-yum install -y java-1.6.0-openjdk
-
-# install tomcat
-echo installing tomcat
-yum install -y tomcat6
+echo installing java 8
+sudo yum install -y java-1.8.0-openjdk.x86_64
 
 # install git
-echo installing tomcat
-yum install -y git
+echo installing git
+sudo yum install -y git
+
+# Download DMC Solr files
+cd /tmp
+rm -fr /tmp/dmcsolr
+git clone https://bitbucket.org/DigitalMfgCommons/dmcsolr.git
+cd dmcsolr
 
 # Unpacking solr configuration
 rm -fr /tmp/solr
 mkdir /tmp/solr
 cd /tmp/solr
-tar xvfz /tmp/dmcsolr/files/solr.tar.gz
+tar xvfz /tmp/dmcsolr/files/solr5.tar.gz
 
-# download solr
+# download solr 
 echo Downloading and installing solr
-rm -fr /opt/solr
-mkdir /opt/solr
+sudo rm -fr /opt/solr
+sudo mkdir /opt/solr
 cd /opt/solr
-wget http://archive.apache.org/dist/lucene/solr/3.6.0/apache-solr-3.6.0.tgz
-tar xvzf apache-solr-3.6.0.tgz
+sudo wget http://archive.apache.org/dist/lucene/solr/5.3.1/solr-5.3.1.tgz 
+sudo tar xvzf solr-5.3.1.tgz 
 
-#
-echo "Moving solr.war and creating home/solr for configuration..."
-/bin/cp -f /opt/solr/apache-solr-3.6.0/example/webapps/solr.war /usr/share/tomcat6/webapps/solr.war
-cd /opt/solr/apache-solr-3.6.0
-mkdir home
-mkdir home/solr
+# Note home directory for Solr is
+# /opt/solr/solr-5.3.1/server/solr
 
-# Moving configuration directories
-mv -f /tmp/solr/LuceneSolrConfig/* /opt/solr/apache-solr-3.6.0/home/solr
-mv -f /tmp/solr/TomcatConfig/server.xml /usr/share/tomcat6/conf/server.xml
+# Moving configuration directories to solr home
+echo moving DMC solr configuration files to solr home /opt/solr/solr-5.3.1/server/solr
+sudo mv -f /tmp/solr/LuceneSolrConfig/* /opt/solr/solr-5.3.1/server/solr
 
 # Edit components.data-config.xml
 cd /tmp/dmcsolr
-sed "s/SOLR_DB_DNS/$solrDbDns/" files/components.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/components/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/components.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/components/conf/data-config.xml
 
 # Edit projects.data-config.xml
-sed "s/SOLR_DB_DNS/$solrDbDns/" files/projects.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/projects/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/projects.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/projects/conf/data-config.xml
 
 # Edit services.data-config.xml
-sed "s/SOLR_DB_DNS/$solrDbDns/" files/services.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/services/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/services.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/services/conf/data-config.xml
 
 # Edit users.data-config.xml
-sed "s/SOLR_DB_DNS/$solrDbDns/" files/users.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/users/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/users.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/users/conf/data-config.xml
 
 # Edit wiki.data-config.xml
-sed "s/SOLR_DB_DNS/$solrDbDns/" files/wiki.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/wiki/conf/data-config.xml
+sudo sed "s/SOLR_DB_DNS/$solrDbDns/" files/wiki.data-config.xml > /opt/solr/apache-solr-3.6.0/home/solr/wiki/conf/data-config.xml
 
 #
-echo "chowning directories"
-cd /opt/solr/apache-solr-3.6.0/home/solr 
-chown tomcat . components components/conf  services services/conf projects projects/conf users users/conf artifacts artifacts/conf wiki wiki/conf
+echo starting solr
+sudo /opt/solr/apache-solr-3.6.0/bin/solr start
 
-#
-echo "Starting Apache Tomcat" 
-/etc/rc.d/init.d/tomcat6 start
-chkconfig tomcat6 on
 
