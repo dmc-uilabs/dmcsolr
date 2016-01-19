2fac36f1613714a684202fa0d5c0235fce955257
Wise <200005921@logon.ds.ge.com>
2016-01-19 15:08:08 -0500

    Fixed typo in indexing script to use projects instead of products

--- files/scripts/solr_fullindex.sh
+++ files/scripts/solr_fullindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl  "http://localhost:8983/solr/gforge_components/dataimport?command=full-import&clean=true" 
-curl  "http://localhost:8983/solr/gforge_products/dataimport?command=full-import&clean=true" 
+curl  "http://localhost:8983/solr/gforge_projects/dataimport?command=full-import&clean=true" 
 curl  "http://localhost:8983/solr/gforge_services/dataimport?command=full-import&clean=true" 
 curl  "http://localhost:8983/solr/gforge_users/dataimport?command=full-import&clean=true" 
 curl  "http://localhost:8983/solr/gforge_wiki/dataimport?command=full-import&clean=true" 
