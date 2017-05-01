255ed849639adebe173d6c27efea735fb0b5ec7e
alex maties <alex.maties@uilabs.org>
2017-04-30 23:44:51 -0500

    cleanup and license

--- updateMe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd DMC
-rm -rf *
-git clone https://bitbucket.org/DigitalMfgCommons/dmcrestservices.git
-cd dmcrestservices
-#mvn package
-cp target/dmc-site-services-0.1.0.war /var/lib/tomcat7/webapps
-/etc/init.d/tomcat7 restart
